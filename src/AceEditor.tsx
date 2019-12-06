f9267a9fd456a4e2cf5c5d22f03a25cb5bd7be2a
luger <stefan.luger@customweb.com>
2019-12-06 17:50:43 +0100

    custom completer

--- src/AceEditor.tsx
+++ src/AceEditor.tsx
@@ -1,50 +1,146 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import * as Ace from 'brace';
+import * as ace from 'ace-builds';
 
-import 'brace/mode/sql';
-import 'brace/theme/eclipse';
-import 'brace/ext/language_tools';
+import 'ace-builds/src-noconflict/mode-sql';
+import 'ace-builds/src-noconflict/theme-eclipse';
+import 'ace-builds/src-noconflict/ext-language_tools';
 
-export interface AceEditorOptions {
+
+export interface IAceEditorOptions {
     minLines: number;
     maxLines: number;
     enableBasicAutocompletion: boolean;
+    mode: string;
+    theme: string;
+    showPrintMargin: boolean;
 }
 
-interface AceEditorProps {
+interface IAceEditorProps {
     code: string;
-    options?: Partial<AceEditorOptions>;
+    options?: Partial<IAceEditorOptions>;
 }
 
-export class AceEditor extends React.Component<AceEditorProps, {}> {
-    private aceRef: any;
-    private options: Partial<AceEditorOptions> = {};
+export class CustomHighlightRules extends ace.require("ace/mode/text_highlight_rules").TextHighlightRules {
+    constructor() {
+        super();
 
-    constructor(props: AceEditorProps) {
+        // TODO: define prestosql allowed keywords
+        var keywords = (
+            "select|from|where|and|or|group|by|order|limit|offset|having|as|case|" +
+            "when|then|else|end|type|left|right|join|on|outer|desc|asc|union|create|table|primary|key|if|" +
+            "foreign|not|references|default|null|inner|cross|natural|database|drop|grant"
+        );
+
+        // TODO: ...
+        var builtinConstants = (
+            "true|false"
+        );
+
+        // TODO: ...
+        var builtinFunctions = (
+            "AVG|COUNT|FIRST|last|max|min|sum|ucase|lcase|mid|len|round|rank|now|format|" +
+            "coalesce|ifnull|isnull|nvl"
+        );
+
+        // TODO: ...
+        var dataTypes = (
+            "int|numeric|decimal|date|varchar|char|bigint|float|double|bit|binary|text|set|timestamp|" +
+            "money|real|number|integer"
+        );
+
+        var keywordMapper = this.createKeywordMapper({
+            "support.function": builtinFunctions,
+            "keyword": keywords,
+            "constant.language": builtinConstants,
+            "storage.type": dataTypes,
+        }, "identifier", true);
+
+        this.$rules = {
+            "start": [{
+                token: "comment",
+                regex: "--.*$"
+            }, {
+                token: "comment",
+                start: "/\\*",
+                end: "\\*/"
+            }, {
+                token: "string",           // " string
+                regex: '".*?"'
+            }, {
+                token: "string",           // ' string
+                regex: "'.*?'"
+            }, {
+                token: "string",           // ` string (apache drill)
+                regex: "`.*?`"
+            }, {
+                token: "constant.numeric", // float
+                regex: "[+-]?\\d+(?:(?:\\.\\d*)?(?:[eE][+-]?\\d+)?)?\\b"
+            }, {
+                token: keywordMapper,
+                regex: "[a-zA-Z_$][a-zA-Z0-9_$]*\\b"
+            }, {
+                token: "keyword.operator",
+                regex: "\\+|\\-|\\/|\\/\\/|%|<@>|@>|<@|&|\\^|~|<|>|<=|=>|==|!=|<>|="
+            }, {
+                token: "paren.lparen",
+                regex: "[\\(]"
+            }, {
+                token: "paren.rparen",
+                regex: "[\\)]"
+            }, {
+                token: "text",
+                regex: "\\s+"
+            }]
+        };
+    }
+}
+export default class CustomSqlMode extends ace.require('ace/mode/text').Mode {
+    constructor() {
+        super();
+        this.HighlightRules = CustomHighlightRules;
+    }
+}
+
+export class AceEditor extends React.Component<IAceEditorProps, {}> {
+    private aceRef: any;
+
+    private options: Partial<IAceEditorOptions> = {};
+
+    private readonly defaultOptions: IAceEditorOptions = {
+        minLines: 10,
+        maxLines: 20,
+        enableBasicAutocompletion: false,
+        mode: 'ace/mode/text',
+        theme: 'ace/theme/eclipse',
+        showPrintMargin: false
+    }
+
+    constructor(props: IAceEditorProps) {
         super(props);
         this.aceRef = null;
-
-        const defaultOptions: AceEditorOptions = {
-            minLines: 10,
-            maxLines: 20,
-            enableBasicAutocompletion: false
-        }
-
-        Object.assign(this.options, defaultOptions, this.props.options);
+        Object.assign(this.options, this.defaultOptions, this.props.options);
     }
 
     componentDidMount() {
         const node = ReactDOM.findDOMNode(this.aceRef) as HTMLDivElement;
-        const editor = Ace.edit(node);
+        const editor = ace.edit(node, this.options);
+        const customMode = new CustomSqlMode();
+        editor.getSession().setMode(customMode as ace.Ace.SyntaxMode);
 
-        editor.getSession().setMode('ace/mode/sql');
-        editor.setTheme('ace/theme/eclipse');
-        editor.setShowPrintMargin(false);
+        const customKeyWordCompleter = {
+            getCompletions(editor: ace.Ace.Editor, session: ace.Ace.EditSession, pos: ace.Ace.Point, prefix: string, callback: ace.Ace.CompleterCallback) {
+                const state = editor.session.getState(pos.row);
+                let keywordCompletions = (session as any).$mode.getCompletions(state, session, pos, prefix);
+                keywordCompletions = keywordCompletions.map((obj: ace.Ace.Completion) => {
+                    return Object.assign(obj, { value: obj.value.toUpperCase() });
+                });
+                return callback(null, keywordCompletions);
+            },
+        };
 
-        Object.entries(this.options).forEach((v: any[]) => {
-            editor.setOption(v[0], v[1]);
-        });
+        // replace lowercase completer with custom uppercase completer
+        editor.completers[2] = customKeyWordCompleter;
     }
 
     render() {
--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import './App.css';
-import { AceEditor, AceEditorOptions } from './AceEditor';
+import { AceEditor, IAceEditorOptions } from './AceEditor';
 
 const App: React.FC = () => {
   const code = `SELECT t.id, t.name, t.amount 
   FROM transactions t 
   WHERE t.id > 5000`;
-  const options: AceEditorOptions = {
+
+  const options: IAceEditorOptions = {
     minLines: 25,
     maxLines: 50,
-    enableBasicAutocompletion: true
+    enableBasicAutocompletion: true,
+    mode: 'ace/mode/sql',
+    theme: 'ace/theme/eclipse',
+    showPrintMargin: false
   }
+
   return (<React.Fragment>
     <h1>Brace yourself</h1>
     <div className="App"><AceEditor code={code} options={options} /></div>
